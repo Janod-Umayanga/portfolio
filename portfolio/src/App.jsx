41fb33daf922475aec74f5ff87f0dd63105ce464
Janod-Umayanga <87465214+Janod-Umayanga@users.noreply.github.com>
2023-08-03 12:42:43 +0530

    made the home route and did the major styles to the page

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Pages
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <h1>Home</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <h1>Welcome Stranger! This is my Portfolio</h1>
+      <p>My name is <strong>Janod Umayanga</strong> and I am a <strong>Computer Science student</strong>. I found my self as a person who works best for things he passionates about to achieve a certain goal. I am passionate about Computer Science, Technology, Inovation and Video Games. And also interested in Music, Movies and TV Shows.</p>
+      <p>My goal is to become a <strong>Software Engineer</strong> and to be able to work in a company that is passionate about what they do and to be able to work with people who are passionate about what they do.</p>
+      <h2>Education</h2>
+      <hr/>
+    </div>
+  )
+}
+
+export default Home
